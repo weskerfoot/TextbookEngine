28abba89226c9a5e1747d4a65a3bc5c8a730790b
wes <wjak56@gmail.com>
2016-06-20 04:15:41 -0400

    move files to src/ directory

---
+++ src/goasearch.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python2
+
+# predictive data
+# switch to elasticsearch's prediction
+
+
+
+import database
+import predictions
+
+class GOASearch(object):
+    def __init__(self):
+        return self
+
