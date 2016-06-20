dc1d0c87b28eacb1fdf9866e74c74e91d2c565de
wes <wjak56@gmail.com>
2016-06-20 04:14:28 -0400

    Explain the purpose of the Racket files

--- course_mapping.rkt
+++ course_mapping.rkt
@@ -1,5 +1,11 @@
 #! /usr/bin/racket
 #lang racket
+; This file is used to generate the mapping for elasticsearch
+; It is written in Racket (a dialect of Scheme)
+; It will not be necessary to run unless you want to change the elasticsearch mapping
+; This may be necessary if you have fields you want to add, or need some other customization
+; You may also edit the JSON mapping directly, or use whatever tool you want to edit the mapping with
+
 (require "schemadsl.rkt")
 
 (displayln
