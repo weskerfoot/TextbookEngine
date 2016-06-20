432f4e78ab1fd107f1c9e1f059a4e4cfaaf9a9b6
wes <wjak56@gmail.com>
2016-06-20 04:16:06 -0400

    remove redundant files

--- mcmaster/site.py
+++
@@ -1,9 +0,0 @@
-from oersearch import Search
-from classes import getCourses
-from sylla import getTextbooks
-
-mcmasterSearch = Search("McMaster")
-
-mcmasterSearch.setup(getCourses)
-
-mcmasterSearch.run()
