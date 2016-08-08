943d9a8facd07516fb3454a7f669320a8ed8e262
wes <wjak56@gmail.com>
2016-08-08 17:00:34 -0400

    loading thingy

--- src/scripts/search.js
+++ src/scripts/search.js
@@ -48,7 +48,7 @@ function ResultsPasser() {
 
 var results_passer = new ResultsPasser();
 
-riot.mount("search");
+riot.mount("search", {booksLoading : false});
 riot.mount("results");
 
 function autocomplete(element, endpoint) {
